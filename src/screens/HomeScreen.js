0db3a4f49437227343a80a6122df3deb7eaa5c77
Turjmann <turjumant98@gmail.com>
2021-06-01 01:59:22 +0300

    1:59 AM 01/06

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,21 +1,32 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, ImageBackground } from 'react-native';
+import styled from 'styled-components'
+import Text from '../components/Text'
+import { FirebaseContext } from '../context/FirebaseContext'
+import { UserContext } from '../context/UserContext'
 
+
+
+const [user, setUser] = useContext(UserContext)
+const firebase = useContext(FirebaseContext)
+const [userData, setUserData] = useState(null);
 
 export default HomeScreen = () => {
 
     return (
-        <View style = {styles.container}>
-            <Text>Home Screen</Text>
-        </View>
+        
+        <Container>
+            
+            <Text title semi center>Hello there</Text>
+              
+        </Container>
+
+        
     )
 }
 
+const Container = styled.View`
+    flex: 1;
+    justify-content: center;
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
+`
